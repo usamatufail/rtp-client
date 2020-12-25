85432e67066510a3cf8d6f3a9aa1f42551365e3c
Usama Tufail <miansamusama@gmail.com>
2020-12-25 18:31:06 +0500

    ci(firebase config): changed configuration to use local

--- src/firebase-config/index.js
+++ src/firebase-config/index.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDBj6AwpO7VvohoF4tIJj0EXyAhXgz7vvc",
+  authDomain: "rich-to-poor.firebaseapp.com",
+  databaseURL: "https://rich-to-poor.firebaseio.com",
+  projectId: "rich-to-poor",
+  storageBucket: "rich-to-poor.appspot.com",
+  messagingSenderId: "460258280746",
+  appId: "1:460258280746:web:dff1c340ea1f9b6bf24a53",
+  measurementId: "G-8J6RER8YKC",
 });
 
 export const auth = app.auth();
